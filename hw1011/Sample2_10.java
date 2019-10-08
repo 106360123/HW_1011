1cc27ba496acda30c126f060d01a3805071ac2bd
abbyhuang0726 <abbyhuang0726@gmail.com>
2019-10-09 01:08:24 +0800

    first commit

---
+++ hw1011/Sample2_10.java
@@ -0,0 +1,14 @@
+package hw1011;
+
+public class Sample2_10 
+{
+	public static void main(String[]args)
+	{
+		int i=1;
+		while(i<=5) {
+			System.out.println("第"+i+"次的迴圈");
+			i++;
+		}
+		System.out.println("迴圈結束");
+	}
+}
